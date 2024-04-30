e25aa53ce5d6e56631e19318aff3ca8bed41ad42
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-05-01 00:53:27 +0300

    my first commit

---
+++ my-gallery/src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>My Images</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ my-gallery/src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Gallery</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/liked">Liked</a>
+        <a href="/upload">Upload</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
