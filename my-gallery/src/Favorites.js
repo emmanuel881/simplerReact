c5a0802d71f91be9f314065b215b05ad380944a4
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-05-04 14:19:47 +0300

    favorite page

--- my-gallery/src/Favorites.js
+++ my-gallery/src/Favorites.js
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import useFetch from "./useFetch";
+import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Favorites = () => {
+  const history = useHistory();
   const {
     data: images,
     error,
     isPending,
   } = useFetch("http://localhost:8000/images");
+  const removeFav = async (id) => {
+    const resource = "http://localhost:8000/images/" + id;
+    try {
+      const updatedData = { id: id, fav: false };
+
+      await axios.patch(resource, updatedData);
+
+      console.log("Removed from favorite list");
+      history.push("Upload");
+    } catch (error) {
+      console.log("Error updating JSON file: ", error);
+    }
+  };
+
   return (
     <div className="fav">
       <h1>My Favorites Images</h1>
@@ -21,6 +39,7 @@ const Favorites = () => {
                   style={{
                     backgroundColor: "#e83336",
                   }}
+                  onClick={() => removeFav(image.id)}
                 >
                   Remove
                 </button>
