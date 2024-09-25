4f8952e992a8ba8731615daf45f0f713e0b4b5ef
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-09-25 22:19:02 +0300

    project setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@
 import "./App.css";
 import CakeView from "./features/cake/CakeView";
 import IceCreamView from "./features/icecream/IceCreamView";
+import UserView from "./features/user/UserView";
 
 function App() {
   return (
     <div className="App">
       <CakeView />
       <IceCreamView />
+      <UserView />
     </div>
   );
 }
---
+++ my-app/src/features/user/UserView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserView = () => {
+  return (
+    <div>
+      <h1>List of users </h1>
+    </div>
+  );
+};
+
+export default UserView;
